1fc3e7c7406c73fdbd5422d74fcde337b3da652b
Andy Sayler <andy.sayler@gmail.com>
2013-09-11 10:17:18 -0600

    Expnaded build script

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
 packages=( */debian )
+failure=0
 
 for p in "${packages[@]}"
 do
     pushd "${p}" > /dev/null
     rm -f *.deb
     rm -f *.dsc
-    equivs-build -f "control"
+    echo "Building ${p}..."
+    equivs-build -f "control" > /dev/null
+    if [$? -ne 0]
+    then
+	echo "Build failed"
+	failure=1
+    else
+	echo "Build succeeded"
+    fi
     popd > /dev/null
 done
+
+exit ${failure}
