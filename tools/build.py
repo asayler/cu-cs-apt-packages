2b36499244c411a175d7e155e00c4b80c641679f
Andy Sayler <andy.sayler@gmail.com>
2014-12-12 16:02:28 -0700

    Added build command shell

--- tools/build.py
+++ tools/build.py
@@ -3,8 +3,22 @@
 import click
 import debian.changelog
 
-def cli():
-    print("Not yet implemented")
+_DEFAULT_BUILD_DIR = "/tmp/cu-cs-pkg-build"
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    pass
+
+@click.command()
+@click.option('--package', default=None, type=str, help="Package to build (Defaults to All)")
+@click.option('--build_dir', default=_DEFAULT_BUILD_DIR, type=str, help="Build Directory")
+@click.pass_obj
+def build(obj, package, build_dir):
+    pass
+
+# CLI Commands
+cli.add_command(build)
 
 if __name__ == '__main__':
     cli()
