a1ba0c3f4dec216b90f3a89aa7f1c8d5d6476b7b
Andy Sayler <andy.sayler@gmail.com>
2014-12-12 23:57:04 -0700

    Updated cs-cs-apt-other for Spring 15

--- trusty/cu-cs-apt-other/debian/postrm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Andy Sayler
-# cu-cs-apt-other
-# Post-remove Script
-
-echo "Nothing to cleanup"
-exit 0
--- trusty/cu-cs-apt-other/debian/preinst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Andy Sayler
-# cu-cs-apt-other
-# Pre-install Script
-
-echo "Nothing to setup"
-exit 0
