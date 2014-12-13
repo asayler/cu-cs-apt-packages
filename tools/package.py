59a47f48509bf54e3595f4471ff68d40eee1a6e5
Andy Sayler <andy.sayler@gmail.com>
2014-12-13 15:38:25 -0700

    Added publish

--- tools/package.py
+++ tools/package.py
@@ -151,19 +151,134 @@ def download(obj, urls_file):
 
 @click.command()
 @click.option('--repo_dir', default=_DEFAULT_REPO_DIR, type=str, help="Path to Repo")
-@click.option('--release', default=None, type=str, help="Release to Publish")
+@click.option('--release', prompt=True, type=str, help="Release to Publish")
+@click.option('--major_vers', default=None, type=str, help="Versions to Publish")
 @click.pass_obj
-def publish(obj, repo_dir, release):
+def publish(obj, repo_dir, release, major_vers):
 
+    # Setup Paths
     build_dir = obj['build_dir']
     deb_out_dir = os.path.join(build_dir, _BUILD_PKG_DIR)
     repo_dir = os.path.abspath(repo_dir)
-    
+
+    def dpkg_field(pkg_path, field):
+        cmd = ["dpkg", "-f", "{}".format(pkg_path), "{}".format(field)]
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        if proc.returncode:
+            raise click.ClickException("Dpkg Failed")
+        return out.decode(_ENCODING).lstrip().rstrip()
+
+    def get_pkg_info(pkg_path):
+        # ToDo Replace dpkg calls with native python deb interface
+        name = dpkg_field(pkg_path, 'Package')
+        vers = dpkg_field(pkg_path, 'Version')
+        arch = dpkg_field(pkg_path, 'Architecture')
+        return (name, vers, arch)
+
+    def get_repo_info(pkg_name, pkg_arch, release, attribute):
+        cmd = ["reprepro", "-T", "deb", "-A", "{}".format(pkg_arch),
+               "--list-format", "'${{{}}}'".format(attribute),
+               "list", "{}".format(release), "{}".format(pkg_name)]
+        proc = subprocess.Popen(cmd, cwd=repo_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        if err:
+            click.secho(err.decode(_ENCODING), err=True, fg='red')
+        if proc.returncode:
+            raise click.ClickException("reprepro List Failed")
+        return out.decode(_ENCODING).lstrip().rstrip().lstrip("'").rstrip("'")
+
+    def repo_publish_deb(pkg_path, release):
+        cmd = ["reprepro", "-s", "includedeb",
+               "{}".format(release), "{}".format(pkg_path)]
+        proc = subprocess.Popen(cmd, cwd=repo_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        if err:
+            click.secho(err.decode(_ENCODING), err=True, fg='red')
+        if proc.returncode:
+            raise click.ClickException("reprepro Publish Failed")
+        return out.decode(_ENCODING).lstrip().rstrip()
+
+    def repo_publish_dsc(pkg_path, release):
+        cmd = ["reprepro", "-s", "includedsc",
+               "{}".format(release), "{}".format(pkg_path)]
+        proc = subprocess.Popen(cmd, cwd=repo_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        if proc.returncode:
+            click.secho(err.decode(_ENCODING), err=True, fg='red')
+            raise click.ClickException("reprepro Publish Failed")
+        return out.decode(_ENCODING).lstrip().rstrip()
+
+    # Find Packages
+    deb_packages = []
+    dsc_packages = []
+    for root, dirs, files in os.walk(deb_out_dir):
+        for fle in files:
+            path = os.path.join(root, fle)
+            ext = os.path.splitext(path)[1]
+            if ext == ".deb":
+                deb_packages.append(path)
+            elif ext == ".dsc":
+                dsc_packages.append(path)
+            else:
+                pass
+
+    # Publish Packages
+    failed = []
+    succeeded = []
+    skipped = []
+    for pkg_path in deb_packages:
+
+        # Get Package Info
+        pkg_name, pkg_vers, pkg_arch = get_pkg_info(pkg_path)
+        click.secho("Publishing {}...".format(pkg_name), fg='blue')
+
+        # Filter Packages
+        if major_vers:
+            pkg_major_vers = pkg_vers.split('.')[0]
+            if pkg_major_vers != major_vers:
+                click.secho("Skipping: Wrong Major Version {}".format(pkg_major_vers), fg='yellow')
+                skipped.append(pkg_name)
+                continue
+
+        # Get Repo Version
+        if pkg_arch == "all":
+            repo_vers_amd64 = get_repo_info(pkg_name, "amd64", release, "version")
+            repo_vers_i386 = get_repo_info(pkg_name, "i386", release, "version")
+            if repo_vers_amd64 != repo_vers_i386:
+                click.secho("Architecture Version Mismatch", err=True, fg='red')
+                failed.append(pkg_name)
+                continue
+            else:
+                repo_vers = repo_vers_amd64
+        else:
+            repo_vers = get_repo_info(pkg_name, pkg_arch, release, "version")
+
+        # Compare Versions
+        if pkg_vers == repo_vers:
+            click.secho("Skipping: Version {} already in repo.".format(pkg_vers), fg='yellow')
+            skipped.append(pkg_name)
+            continue
+
+        # Publish
+        repo_publish_deb(pkg_path, release)
+        click.secho("Publishing {} Succeeded".format(pkg_name), fg='green')
+        succeeded.append(pkg_name)
+
+    # Print Success/Failure
+    click.secho("")
+    if succeeded:
+        click.secho("Succeeded Packages:\n{}".format(succeeded), fg='green')
+    if skipped:
+        click.secho("Skipped Packages:\n{}".format(skipped), fg='yellow')
+    if failed:
+        click.secho("Failed Packages:\n{}".format(failed), err=True, fg='red')
 
 # CLI Commands
 cli.add_command(clean)
 cli.add_command(build)
 cli.add_command(download)
+cli.add_command(publish)
 
 if __name__ == '__main__':
     cli()
