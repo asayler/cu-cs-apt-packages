02e516bd53b7a17fe95f70e69c5871fcd393d46d
Andy Sayler <andy.sayler@gmail.com>
2014-12-12 21:26:32 -0700

    Added build and pgp support

--- tools/package.py
+++ tools/package.py
@@ -8,17 +8,24 @@ import shutil
 import click
 import debian.changelog
 
+_ENCODING = "utf-8"
+
 _DEFAULT_BUILD_DIR = "/tmp/cu-cs-pkg-build"
 _DEFAULT_SOURCE_DIR = "./"
 
-_PACKAGE_DIR = "debian"
+_DEB_DIR = "debian"
 _BUILD_SRC_DIR = "src"
-_BUILD_PGK_DIR = "pkg"
+_BUILD_PKG_DIR = "pkg"
 
 @click.group()
+@click.option('--gpg_dir', default=None, type=str, help="GPG Home Directory")
 @click.pass_context
-def cli(ctx):
-    pass
+def cli(ctx, gpg_dir):
+    
+    if gpg_dir:
+        gpg_dir = os.path.abspath(gpg_dir)
+        click.secho("Setting GNUPGHOME to '{}'".format(gpg_dir))
+        os.environ['GNUPGHOME'] = gpg_dir
 
 @click.command()
 @click.option('--source_dir', default=_DEFAULT_SOURCE_DIR, type=str, help="Source Directory")
@@ -44,11 +51,36 @@ def build(obj, source_dir, build_dir):
     # Find Packages
     packages = []
     for root, dirs, files in os.walk(export_dir):
-        pkg_dir = os.path.join(root, _PACKAGE_DIR)
+        pkg_dir = os.path.join(root, _DEB_DIR)
         if os.path.isdir(pkg_dir):
             packages.append(root)
     print(packages)
-    
+
+    # Build Packages
+    succeeded = []
+    failed = []
+    deb_out_dir = os.path.join(build_dir, _BUILD_PKG_DIR)
+    cmd = ["equivs-build", "-f", "control"]
+    for pkg_src_dir in packages:
+        pkg_name = os.path.basename(pkg_src_dir)
+        click.secho("Building {}...".format(pkg_name), fg='blue')
+        deb_src_dir = os.path.join(pkg_src_dir, _DEB_DIR)
+        proc = subprocess.Popen(cmd, cwd=deb_src_dir,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        if proc.returncode:
+            click.secho("{} Build Failed".format(pkg_name), err=True, fg='red')
+            click.secho(stderr.decode(_ENCODING), err=True, fg='red')
+            failed.append(pkg_name)
+        else:
+            click.secho("{} Build Succeeded".format(pkg_name), fg='green')
+            succeeded.append(pkg_name)
+
+    if succeeded:
+        click.secho("Succeeded Packages: {}".format(succeeded), fg='green')
+    if failed:
+        click.secho("Failed Packages: {}".format(failed), err=True, fg='red')
+        
     
 # CLI Commands
 cli.add_command(build)
